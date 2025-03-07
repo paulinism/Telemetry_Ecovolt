00403d5c1e5a18b7edbb98a1a7cbd96c139244f6
JДиего03 <151983723+josediegogf03@users.noreply.github.com>
2025-03-07 09:18:00 -0600

    Update SensorCurr_int.ino

--- Current_sensor_int/SensorCurr_int.ino
+++ Current_sensor_int/SensorCurr_int.ino
@@ -33,8 +33,7 @@ void setup_timer(){
   timerAlarm(timer, 500000, true, 0); // Interruption every 1/2 second
 }
 
-float fmap(float x, float in_min, float in_max, float out_min, float out_max)
-{
+float fmap(float x, float in_min, float in_max, float out_min, float out_max){
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
